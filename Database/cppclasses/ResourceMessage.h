b4161c6758355a69e90d1b4b59a19152651b001f
Matthew Guidry <mguid2088@gmail.com>
2018-10-16 15:21:11 -0500

    Refactor ResourceMessage.h

--- Database/cppclasses/ResourceMessage.h
+++ Database/cppclasses/ResourceMessage.h
@@ -1,21 +1,33 @@
 #include <string>
-#pragma once
-using namespace std;
-class ResourceMessage
-{
+
+#ifndef RESOURCEMESSAGE_H
+#define RESOURCEMESSAGE_H
+
+class ResourceMessage {
 public:
-	ResourceMessage(string messageID);
-	~ResourceMessage();
-	string MessageID;
-	int SentDateTime;
-	string MessageContentType;
-	string MessageDescription;
-	string OriginatingMessageID;
-	string PrecedingMessageID;
-	string IncidentID;
-	string RecalledMessageID;
-	string FundCode;
-	string ContactInformationID;
-	string ResourceInfoElementID;
+  ResourceMessage() {}
+  ResourceMessage(std::string MessageID, long SentDateTime=0L, std::string MessageContentType="",
+                  std::string MessageDescription, std::string OriginatingMessageID="", std::string PrecedingMessageID="",
+                  std::string IncidentID="", std::string RecalledMessageID="", std::string FundCode="",
+                  std::string ContactInformationID="", std::string ResourceInfoElementID="") :
+                  MessageID(MessageID), SentDateTime(SentDateTime), MessageContentType(MessageContentType),
+                  MessageDescription(MessageDescription), OriginatingMessageID(OriginatingMessageID), PrecedingMessageID(PrecedingMessageID),
+                  IncidentID(IncidentID), RecalledMessageID(RecalledMessageID), FundCode(FundCode),
+                  ContactInformationID(ContactInformationID), ResourceInfoElementID(ResourceInfoElementID) {}
+
+  ~ResourceMessage() {}
+
+  std::string MessageID;
+  long SentDateTime;
+  std::string MessageContentType;
+  std::string MessageDescription;
+  std::string OriginatingMessageID;
+  std::string PrecedingMessageID;
+  std::string IncidentID;
+  std::string RecalledMessageID;
+  std::string FundCode;
+  std::string ContactInformationID;
+  std::string ResourceInfoElementID;
 };
 
+#endif /* RESOURCEMESSAGE_H */
