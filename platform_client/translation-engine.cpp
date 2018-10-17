ac26ec30489b1d1c14f123b1d45e3ddd8c1e199f
Matthew Guidry <mguid2088@gmail.com>
2018-10-16 20:14:29 -0500

    Add method promises

--- platform_client/translation-engine.cpp
+++ platform_client/translation-engine.cpp
@@ -1,1 +1,27 @@
-#include ""
+
+
+RequestResource TranslationEngine::xml_to_request_response_msg(std::string xml_str) {
+  pugi::xml_document doc;
+  pugi::xml_parse_result result = doc.load_string(xml_str);
+
+  
+}
+
+/*
+ResponseToRequestResource TranslationEngine::xml_to_response_to_request_resource_msg(std::string xml_str) {
+
+
+}
+*/
+
+std::string TranslationEngine::resource_request_msg_to_xml(RequestResource msg) {
+  pugi::xml_document doc;
+
+  
+}
+
+/*
+std::string TranslationEngine::resource_request_msg_to_xml(ResponseToRequestResource msg) {
+
+}
+*/
--- platform_client/translation-engine.h
+++ platform_client/translation-engine.h
@@ -1,6 +1,6 @@
 #include "./dependencies/json.hpp"
 #include "./dependencies/pugixml.hpp"
-#include ""
+#include "./message_elements/ResourceRequest.h"
 #include <string>
 
 class TranslationEngine {
@@ -8,8 +8,8 @@ public:
   TranslationEngine() {}
   ~TranslationEngine() {}
 
-  xml_to_resource_request_msg(std::string xml_str);
-  xml_to_response_to_resource_request_msg(std::string xml_str);
-  std::string resource_request_msg_to_xml();
-  //std::string response_to_resource_request_msg_to_xml();
+  RequestResource xml_to_request_response_msg(std::string xml_str);
+  //ResponseToRequestResource xml_to_response_to_request_resource_msg(std::string xml_str);
+  std::string resource_request_msg_to_xml(RequestResource msg);
+  //std::string response_to_request_resource_msg_to_xml(ResponseToRequestResource msg);
 }
