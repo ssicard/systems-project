8df28e4ca16603ca499a57eccb5863904db37d91
Matthew Guidry <mguid2088@gmail.com>
2018-10-16 19:55:54 -0500

    Hack Makefile

--- platform_client/translation-engine.h
+++ platform_client/translation-engine.h
@@ -1,4 +1,6 @@
 #include "./dependencies/json.hpp"
+#include "./dependencies/pugixml.hpp"
+#include ""
 #include <string>
 
 class TranslationEngine {
@@ -8,5 +10,6 @@ public:
 
   xml_to_resource_request_msg(std::string xml_str);
   xml_to_response_to_resource_request_msg(std::string xml_str);
-  std::string msg_to_xml();
+  std::string resource_request_msg_to_xml();
+  //std::string response_to_resource_request_msg_to_xml();
 }
