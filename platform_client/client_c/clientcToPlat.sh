9fbdd4c0684059853dc054dad0d4bad6acf4c6e0
Ubuntu <ubuntu@ip-172-31-23-254.us-west-2.compute.internal>
2018-11-11 23:30:29 +0000

    clientc to platform script complete

---
+++ platform_client/client_c/clientcToPlat.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#====== Set clientc variables ======
+MYSQL_CLIENTC_USER="platform"
+MYSQL_CLIENTC_PASS="psql-password1234"
+MYSQL_HOST="172.31.19.66"
+MYSQL_CLIENTCDUMP="mysqldump -u ${MYSQL_CLIENTC_USER} -p${MYSQL_CLIENTC_PASS} -h ${MYSQL_HOST}"
+MYSQL_CLIENTC="mysql -u ${MYSQL_CLIENTC_USER} -p${MYSQL_CLIENTC_PASS} -h ${MYSQL_HOST}"
+
+#====== Set platform variables ======
+MYSQL_PLATFORM_USER=root
+MYSQL_PLATFORM_PASS=password
+MYSQL_PLAT="mysql -u ${MYSQL_PLATFORM_USER} -p${MYSQL_PLATFORM_PASS}"
+LAST_CLIENTC_CHECK=0
+
+#====== Actual Script ======
+#${MYSQL_CLIENTCDUMP} clientcDB > clientcDB.sql
+#${MYSQL_PLAT} -e "DROP DATABASE IF EXISTS clientcDB;"
+#${MYSQL_PLAT} -e "CREATE DATABASE clientcDB;"
+#${MYSQL_PLAT} clientcDB < clientcDB.sql
+NEW_MESSAGES_TALLY=$(${MYSQL_PLAT} -ss -e 'SELECT COUNT(*) FROM clientcDB.ResourceMessage WHERE SentDateTime > 0;')
+if [ "$NEW_MESSAGES_TALLY" -eq 0 ]
+then 
+   echo "No new messages for clientc"
+else
+   ${MYSQL_PLAT} -e "INSERT INTO progDB.IncidentInformation SELECT DISTINCT(IncidentInformation.IncidentID), IncidentDescription FROM clientcDB.ResourceMessage, clientcDB.IncidentInformation WHERE clientcDB.ResourceMessage.SentDateTime > ${LAST_CLIENTC_CHECK} AND clientcDB.ResourceMessage.IncidentID = clientcDB.IncidentInformation.IncidentID; 
+			 
+INSERT INTO progDB.Funding SELECT DISTINCT(Funding.FundCode), FundingInfo FROM clientcDB.ResourceMessage, clientcDB.Funding WHERE clientcDB.ResourceMessage.SentDateTime > ${LAST_CLIENTC_CHECK} AND clientcDB.ResourceMessage.FundCode = clientcDB.Funding.FundCode; 
+
+INSERT INTO progDB.AssignmentInformation SELECT DISTINCT(AssignmentInformation.AssignmentInformationID), Quantity, Restrictions, AnticipatedFunction, PriceQuote, OrderID, AssignmentInstructionsID FROM clientcDB.ResourceMessage, clientcDB.AssignmentInformation, clientcDB.ResourceInformation WHERE clientcDB.ResourceMessage.SentDateTime > ${LAST_CLIENTC_CHECK} AND clientcDB.ResourceMessage.ResourceInfoElementID = clientcDB.ResourceInformation.ResourceInfoElementID AND clientcDB.ResourceInformation.AssignmentInformationID = clientcDB.AssignmentInformation.AssignmentInformationID; 
+			 
+INSERT INTO progDB.ScheduleInformation(ScheduleInformationID, LocationTypeID) SELECT DISTINCT(ScheduleInformation.ScheduleInformationID), LocationTypeID FROM clientcDB.ResourceMessage, clientcDB.ScheduleInformation, clientcDB.ResourceInformation WHERE clientcDB.ResourceMessage.SentDateTime > ${LAST_CLIENTC_CHECK} AND clientcDB.ResourceMessage.ResourceInfoElementID = clientcDB.ResourceInformation.ResourceInfoElementID AND clientcDB.ResourceInformation.ScheduleInformationID = clientcDB.ScheduleInformation.ScheduleInformationID; 
+
+INSERT INTO progDB.Resource(ResourceID, TypeStructureID, TypeInfoID, KeywordID, ResponsiblePartyID, OwnershipInformationID, ResourceStatusID) SELECT DISTINCT(Resource.ResourceID), TypeStructureID, TypeInfoID, KeywordID, ResponsiblePartyID, OwnershipInformationID, ResourceStatusID FROM clientcDB.ResourceMessage, clientcDB.Resource, clientcDB.ResourceInformation WHERE clientcDB.ResourceMessage.SentDateTime > ${LAST_CLIENTC_CHECK} AND clientcDB.ResourceMessage.ResourceInfoElementID = clientcDB.ResourceInformation.ResourceInfoElementID AND clientcDB.ResourceInformation.Resource = clientcDB.Resource.ResourceID; 
+
+INSERT INTO progDB.ResourceInformation(ResourceInfoElementID, ResponseInformation, Resource, AssignmentInformationID, ScheduleInformationID) SELECT DISTINCT(ResourceInformation.ResourceInfoElementID), ResponseInformation, Resource, AssignmentInformationID, ScheduleInformationID FROM clientcDB.ResourceMessage, clientcDB.ResourceInformation WHERE clientcDB.ResourceMessage.SentDateTime > ${LAST_CLIENTC_CHECK} AND clientcDB.ResourceMessage.ResourceInfoElementID = clientcDB.ResourceInformation.ResourceInfoElementID; 
+
+INSERT INTO progDB.ResourceMessage(MessageID, SentDateTime, IncidentID, RecalledMessageID, FundCode, ContactInformationID, ResourceInfoElementID) SELECT DISTINCT(ResourceMessage.MessageID), SentDateTime, IncidentID, RecalledMessageID, FundCode, ContactInformationID, ResourceInfoElementID FROM clientcDB.ResourceMessage WHERE clientcDB.ResourceMessage.SentDateTime > ${LAST_CLIENTC_CHECK};"
+fi
---
+++ platform_client/client_c/deleteFromPlat.sql
@@ -0,0 +1,7 @@
+DELETE FROM ResourceMessage;
+DELETE FROM IncidentInformation;
+DELETE FROM Funding;
+DELETE FROM ResourceInformation;
+DELETE FROM AssignmentInformation;
+DELETE FROM ScheduleInformation;
+DELETE FROM Resource;
