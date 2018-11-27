d33e5c1054f9fa2541d4a5c02f505dfe19d23a0e
Ubuntu <ubuntu@ip-172-31-19-66.us-west-2.compute.internal>
2018-11-27 20:48:23 +0000

    fixed sample messages to match db

--- platform_client/messages/sample_messages/SampleMessageClientC.sql
+++ platform_client/messages/sample_messages/SampleMessageClientC.sql
@@ -6,6 +6,6 @@ AssignmentInformationID, ScheduleInformationID) VALUES (3, 3, 1, 3);
 INSERT INTO ResourceMessage(MessageID, SentDateTime, MessageContentType,
 MessageDescription, OriginatingMessageID, IncidentID, FundCode,
 ContactInformationID, ResourceInfoElementID) 
-VALUES (3, "2018-11-07", "ResourceRequest", "message description", 3, 0, "125", 2, 0),
-	   (4, "2018-11-07", "ResourceRequest", "resource request", 4, 0, "75", 2, 1),
-	   (5, "2018-11-07", "ResourceRequest", "resource request", 5, 2, "50", 2, 2);
+VALUES (3, "2018-11-07", "ResourceRequest", "sample message, delete me", 3, 0, "125", 2, 0),
+	   (4, "2018-11-07", "ResourceRequest", "sample message, delete me", 4, 0, "75", 2, 1),
+	   (5, "2018-11-07", "ResourceRequest", "sample message, delete me", 5, 2, "50", 2, 2);
